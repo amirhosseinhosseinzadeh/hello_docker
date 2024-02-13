35276f691f29867da3138fb68ce08e27cd27e4bd
hosseinzadeh <hosseinzadeh@HOSSEINZADEH-PC>
2024-02-13 17:13:56 +0330

    add docker-compose with envirnoment config base on start up type

--- dockerfile
+++ dockerfile
@@ -2,11 +2,11 @@ FROM mcr.microsoft.com/dotnet/sdk:8.0 AS build-env
 WORKDIR /app
 COPY ./src/HelloDocker.Terminal/HelloDocker.Terminal.csproj ./
 RUN dotnet restore .
-
 COPY src/HelloDocker.Terminal/. ./
 RUN dotnet publish -o pub -c Release
 
 FROM mcr.microsoft.com/dotnet/aspnet:8.0-alpine AS production-env
+ENV SERVE_STATUS=default
 
 WORKDIR /etc/HelloDocker/.
 COPY --from=build-env /app/pub .
--- src/HelloDocker.Terminal/Program.cs
+++ src/HelloDocker.Terminal/Program.cs
@@ -1,12 +1,26 @@
-﻿(bool keepItUp, string answer) = (true, string.Empty);
-while (keepItUp)
+﻿(bool keepItUp, string answer, string serve_env) = (true, string.Empty, Environment.GetEnvironmentVariable("SERVE_STATUS"));
+switch (serve_env)
 {
-	Console.WriteLine("Hello from docker :))) <3");
-	answer = Console.ReadLine();
-	if(answer.Equals("hello",StringComparison.OrdinalIgnoreCase))
-    {
-        keepItUp = false;
-        Console.WriteLine("Bye Bye !");
-        Thread.Sleep(3000);
-    }
+    case "default":
+        while (keepItUp)
+        {
+            Console.WriteLine("Hello from docker :))) <3");
+            answer = Console.ReadLine();
+            if (answer.Equals("hello", StringComparison.OrdinalIgnoreCase))
+            {
+                keepItUp = false;
+                Thread.Sleep(1000);
+                Console.WriteLine("Bye Bye !");
+            }
+        }
+        break;
+    case "loop":
+        while (keepItUp)
+        {
+            Console.WriteLine("Hello from docker :))) <3");
+            Thread.Sleep(1000);
+        }
+        break;
+    default:
+        throw new Exception("SERVE_ENV does not determined properly. right values mus be one of \"loop\" or \"default\"");
 }
